3ea2fc3bb578178e367dc79f892db3ac65783505
Stephen Sauerbrey <stephensauerbrey@ORRHS.ORG>
2021-06-09 10:24:47 -0400

    EVERYTHING WORKING

--- changePage.js
+++ changePage.js
@@ -2,7 +2,4 @@ document.getElementById('signup').addEventListener('click',  function(){
     window.location.href = "signup.html";
 })
 
-document.getElementById('return').addEventListener('click',  function(){
-    window.location.href = "login.html";
-})
 
